81c4f10fb72dc125a2ea377c9cd035919f8b233c
Gbolahan Oladeji <31252159+mo-gbolahan@users.noreply.github.com>
2025-03-19 09:05:16 +0100

    complete the form and default background

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,48 @@
-function App() {
-  return "Banner";
-}
+import { useForm, SubmitHandler } from "react-hook-form";
+import "./App.scss"; // Import the App.scss file
+//components
+
+type FormField = {
+  background: string;
+  bannerText: string;
+};
+
+const App = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormField>();
+
+  const onSubmit: SubmitHandler<FormField> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="form-container">
+      {" "}
+      {/* Add the form-container class */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register("background", { required: "Background is required" })}
+          placeholder="Background"
+        />
+        {errors.background && (
+          <span className="error-message">{errors.background.message}</span>
+        )}
+
+        <input
+          {...register("bannerText", { required: "Banner Text is required" })}
+          placeholder="Banner Text"
+        />
+        {errors.bannerText && (
+          <span className="error-message">{errors.bannerText.message}</span>
+        )}
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
