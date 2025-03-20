3453e6717c1aa56d21f7580818ac6753ffa83681
Gbolahan Oladeji <31252159+mo-gbolahan@users.noreply.github.com>
2025-03-20 17:59:23 +0100

    adding banner text position

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,18 @@ type FormField = {
   bannerText: string;
   fontSize: string;
   fontColor: string;
+  textPosition: string;
 };
 
 const App = () => {
   const [isFormVisible, setIsFormVisible] = useState(true);
-  const [bannerText, setBannerText] = useState("Banner Text");
-  const [bannerBackground, setBannerBackground] = useState<string>();
+  const [bannerText, setBannerText] = useState("Banner Text. eg Volleyball");
+  const [bannerBackground, setBannerBackground] =
+    useState<string>("volleyball");
   const [backgroundURL, setBackgroundURL] = useState<string>();
-  const [fontSize, setFontSize] = useState<string>("24px");
-  const [fontColor, setFontColor] = useState<string>("#000000");
+  const [fontSize, setFontSize] = useState<string>("5rem");
+  const [fontColor, setFontColor] = useState<string>("#b3b3b3");
+  const [textPosition, setTextPosition] = useState<string>("center");
   const {
     state,
     loading,
@@ -42,6 +45,7 @@ const App = () => {
     setBannerBackground(data.background);
     setFontSize(data.fontSize);
     setFontColor(data.fontColor);
+    setTextPosition(data.textPosition);
   };
 
   useEffect(() => {
@@ -55,68 +59,96 @@ const App = () => {
   };
 
   return (
-    <div className="app-container">
-      <div className="form-container">
-        <button onClick={toggleFormVisibility} className="toggle-button">
-          <FontAwesomeIcon icon={isFormVisible ? faEyeSlash : faEye} />
-        </button>
-        {isFormVisible && (
-          <form onSubmit={handleSubmit(onSubmit)} className="form">
-            <div className="form-group">
+    <div className="frame">
+      <div className="app-container">
+        <div
+          className={`form-container ${isFormVisible ? "visible" : "hidden"}`}
+        >
+          <button onClick={toggleFormVisibility} className="toggle-button">
+            <FontAwesomeIcon icon={isFormVisible ? faEyeSlash : faEye} />
+          </button>
+          {isFormVisible && (
+            <form onSubmit={handleSubmit(onSubmit)} className="form">
+              <div className="form-group">
+                <input
+                  {...register("background", {
+                    required: "Background is required",
+                  })}
+                  placeholder="Text that best describe your banner background"
+                />
+                <button type="submit" className="search-button">
+                  <FontAwesomeIcon icon={faSearch} />
+                </button>
+              </div>
+              {errors.background && (
+                <span className="error-message">
+                  {errors.background.message}
+                </span>
+              )}
+
               <input
-                {...register("background", {
-                  required: "Background is required",
+                {...register("bannerText", {
+                  required: "Banner Text is required",
                 })}
-                placeholder="Text that best describe your banner background"
+                placeholder="Banner Text"
+                onChange={(e) => setBannerText(e.target.value)}
               />
-              <button type="submit" className="search-button">
-                <FontAwesomeIcon icon={faSearch} />
-              </button>
-            </div>
-            {errors.background && (
-              <span className="error-message">{errors.background.message}</span>
-            )}
+              {errors.bannerText && (
+                <span className="error-message">
+                  {errors.bannerText.message}
+                </span>
+              )}
 
-            <input
-              {...register("bannerText", {
-                required: "Banner Text is required",
-              })}
-              placeholder="Banner Text"
-              onChange={(e) => setBannerText(e.target.value)}
-            />
-            {errors.bannerText && (
-              <span className="error-message">{errors.bannerText.message}</span>
-            )}
+              <input
+                {...register("fontSize", {})}
+                placeholder="Font Size (e.g., 24px, 2rem)"
+                onChange={(e) => setFontSize(e.target.value)}
+              />
+              {errors.fontSize && (
+                <span className="error-message">{errors.fontSize.message}</span>
+              )}
 
-            <input
-              {...register("fontSize", {})}
-              placeholder="Font Size (e.g., 24px)"
-              onChange={(e) => setFontSize(e.target.value)}
-            />
-            {errors.fontSize && (
-              <span className="error-message">{errors.fontSize.message}</span>
-            )}
+              <input
+                {...register("fontColor", {})}
+                placeholder="Font Color (e.g., #000000, blue)"
+                onChange={(e) => setFontColor(e.target.value)}
+              />
+              {errors.fontColor && (
+                <span className="error-message">
+                  {errors.fontColor.message}
+                </span>
+              )}
 
-            <input
-              {...register("fontColor", {})}
-              placeholder="Font Color (e.g., #000000)"
-              onChange={(e) => setFontColor(e.target.value)}
-            />
-            {errors.fontColor && (
-              <span className="error-message">{errors.fontColor.message}</span>
-            )}
-          </form>
-        )}
-      </div>
-      <div
-        className="banner"
-        style={{
-          background: backgroundURL
-            ? `url(${backgroundURL}) no-repeat center center/cover`
-            : bannerBackground,
-        }}
-      >
-        <h1 style={{ fontSize, color: fontColor }}>{bannerText}</h1>
+              <select
+                {...register("textPosition", {})}
+                defaultValue="center"
+                onChange={(e) => setTextPosition(e.target.value)}
+              >
+                <option value="flex-start">Top</option>
+                <option value="center">Middle</option>
+                <option value="flex-end">Bottom</option>
+                <option value="flex-start flex-start">Top Left</option>
+                <option value="flex-start flex-end">Top Right</option>
+                <option value="center flex-start">Middle Left</option>
+                <option value="center flex-end">Middle Right</option>
+                <option value="flex-end flex-start">Bottom Left</option>
+                <option value="flex-end flex-end">Bottom Right</option>
+              </select>
+            </form>
+          )}
+        </div>
+        <div
+          className="banner"
+          style={{
+            background: backgroundURL
+              ? `url(${backgroundURL}) no-repeat center center/cover`
+              : bannerBackground,
+            justifyContent: textPosition.split(" ")[0],
+            alignItems: textPosition.split(" ")[1] || "center",
+          }}
+        >
+          <h1 style={{ fontSize, color: fontColor }}>{bannerText}</h1>
+        </div>
       </div>
     </div>
   );
