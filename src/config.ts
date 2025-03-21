9c4568ba9d246cf2bd4cf0bf567efcb570cf66e4
Gbolahan Oladeji <31252159+mo-gbolahan@users.noreply.github.com>
2025-03-21 17:20:48 +0100

    include my api key for easy access

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,9 @@
 // Configuration for Pexels API
 
 const API_URL = "https://api.pexels.com/v1/";
-const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY =
+  import.meta.env.VITE_API_KEY ||
+  " IIgzPspnp3LvB15Pm5f0AuvciF0YLoEnH2kTX9gSYWV14dSQClzkYE9S";
 
 const backgroundPicture = `${API_URL}search?query=`;
 const alternateBackgroundPicture = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`;
